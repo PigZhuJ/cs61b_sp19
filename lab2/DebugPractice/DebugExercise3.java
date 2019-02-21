8216c187e5e152d16582b9a1d112a2d59f169514
Shaoyan Zang <36447899+zangsy@users.noreply.github.com>
2019-02-22 02:16:53 +0900

    lab2 is finished

--- lab2/DebugPractice/DebugExercise3.java
+++ lab2/DebugPractice/DebugExercise3.java
@@ -9,7 +9,7 @@ public class DebugExercise3 {
             String foodType = in.readString();
             double cost = in.readDouble();
             int numAvailable = in.readInt();
-            if (foodType.equals("turnip")) {
+            if (foodType.equals("turnip") && numAvailable >= 0) {
                 int newTotal = totalTurnips + numAvailable;
                 totalTurnips = newTotal;
             }
