bb429968eb7e0fbd409375e472343bc5846b2e14
Shaoyan Zang <36447899+zangsy@users.noreply.github.com>
2019-02-26 23:41:36 +0900

    proj1gold is finished

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,60 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TestArrayDequeGold {
+
+    @Test
+    public void test() {
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<Integer>();
+        ArrayDequeSolution<Integer> happy = new ArrayDequeSolution<>();
+        List<String> operations = new ArrayList<>();
+
+        for (int i = 0; i < 100; i += 1) {
+            // instantiate
+            String operation = "";
+            // turn all operations that have been executed to string.
+            for (String op : operations) {
+                operation = operation + "\n" + op;
+            }
+            // 0 for addFirst, 1 for addLast, 2 for removeFirst, 3 for removeLast.
+            int seed = StdRandom.uniform(3);
+            if (seed == 0) {
+                sad.addFirst(i);
+                happy.addFirst(i);
+                Integer sadFirst = sad.get(0);
+                Integer happyFirst = happy.get(0);
+                operations.add("addFirst(" + i + ")");
+                assertEquals(operation + "\n" + "addFirst(" + i + ")", happyFirst, sadFirst);
+            } else if(seed == 1) {
+                // make this seed equals to a int > 4 can check whether
+                // other methods have problems
+                sad.addLast(i);
+                happy.addLast(i);
+                Integer sadLast = sad.get(sad.size() - 1);
+                Integer happyLast = happy.get(happy.size() - 1);
+                operations.add("addLast(" + i + ")");
+                assertEquals(operation + "\n" + "addLast(" + i + ")", happyLast, sadLast); // addLast has problem
+            } else if(seed == 2 && !sad.isEmpty()) {
+                Integer sadRemoveFirst = sad.removeFirst();
+                Integer happyRemoveFirst = happy.removeFirst();
+                operations.add("removeFirst()");
+                assertEquals(operation + "\n" + "removeFirst()", happyRemoveFirst, sadRemoveFirst);
+            } else if(seed == 3 && !sad.isEmpty()) {
+                Integer sadRemoveLast = sad.removeLast();
+                Integer happyRemoveLast = happy.removeLast();
+                operations.add("removeLast()");
+                assertEquals(operation + "\n" + "removeLast()", happyRemoveLast, sadRemoveLast);
+            }
+        }
+
+    }
+
+    /** This main method is optional. */
+    public static void main(String[] args) {
+        jh61b.junit.TestRunner.runTests(TestArrayDequeGold.class);
+    }
+}
