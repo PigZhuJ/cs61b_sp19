82e4422166286c1311790f5c93bf6a9448177bc3
zangsy <fbhduckpears@gmail.com>
2019-03-28 22:19:28 +0900

    clab6 is finished

--- clab6/BubbleGrid.java
+++ clab6/BubbleGrid.java
@@ -1,11 +1,20 @@
+/**
+ * 1. The hit one is 0, it can connect nothing to the ceiling.
+ * 2. The hit one is 1, but it cannot connect to top
+ */
 public class BubbleGrid {
     private int[][] grid;
-
+    private int rowNum;
+    private int colNum;
+    private int ceiling = 0;
+    private int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     /* Create new BubbleGrid with bubble/space locations specified by grid.
      * Grid is composed of only 1's and 0's, where 1's denote a bubble, and
      * 0's denote a space. */
     public BubbleGrid(int[][] grid) {
         this.grid = grid;
+        rowNum = grid.length;
+        colNum = grid[0].length;
     }
 
     /* Returns an array whose i-th element is the number of bubbles that
@@ -13,7 +22,70 @@ public class BubbleGrid {
      * are unique, valid locations in the grid. Must be non-destructive
      * and have no side-effects to grid. */
     public int[] popBubbles(int[][] darts) {
-        // TODO
-        return null;
+        // DONE
+        /* 0th element of uf will be the ceiling. */
+        UnionFind uf = new UnionFind(rowNum * colNum + 1);
+        // If there is a bubble at where dart will hit, set that grid position to 2.
+        for (int[] dart : darts) {
+            if (grid[dart[0]][dart[1]] == 1) {
+                grid[dart[0]][dart[1]] = 2;
+            }
+        }
+        // Union the rest bubbles which will not be hit.
+        // Because those bubbles that will be hit by dart have
+        // been set to 2, can be sure that if the rest bubbles
+        // can still be connected to the ceiling, they will not
+        // fall be sure.
+        for (int row = 0; row < rowNum; row += 1) {
+            for (int col = 0; col < colNum; col += 1) {
+                if (grid[row][col] == 1) {
+                    unionNeighbors(row, col, grid, uf);
+                }
+            }
+        }
+
+        int[] res = new int[darts.length];
+        // Count how many bubbles are connected to the ceiling.
+        int count = uf.sizeOf(ceiling);
+        // Reversely put 1 back to the position that will be hit.
+        for (int i = darts.length - 1; i >= 0; i -= 1) {
+            int row = darts[i][0];
+            int col = darts[i][1];
+            if (grid[row][col] == 2) {
+                unionNeighbors(row, col, grid, uf);
+                grid[row][col] = 1;
+            }
+            int newCount = uf.sizeOf(ceiling);
+            // If newCount > count, it means that there are some bubbles connecting
+            // to the ceiling through the bubble that will hit by dart. So when that
+            // bubble be hit, those "newly" connected bubbles will fall.
+            // newCount - count - 1 because the bubble be hit does not count.
+            res[i] = newCount > count ? newCount - count - 1 : 0;
+            // Update the count, because the put-back bubble might
+            // connects some bubbles to the ceiling.
+            count = newCount;
+        }
+        return res;
     }
+
+    private void unionNeighbors(int row, int col, int[][] grid, UnionFind uf) {
+        for (int[] dir : dirs) {
+            int adjRow = row + dir[0];
+            int adjCol = col + dir[1];
+            if (adjRow < 0 || adjRow == rowNum || adjCol < 0 || adjCol == colNum || grid[adjRow][adjCol] != 1) {
+                continue;
+            }
+            uf.union(xyTo1D(row, col), xyTo1D(adjRow, adjCol));
+        }
+        if (row == 0) {
+            uf.union(xyTo1D(row, col), ceiling);
+        }
+    }
+
+    // Turn (row, col) to uf index.
+    private int xyTo1D(int row, int col) {
+        return row * rowNum + col + 1;
+    }
+
+
 }
--- clab6/UnionFind.java
+++ clab6/UnionFind.java
@@ -1,35 +1,48 @@
 public class UnionFind {
 
-    // TODO - Add instance variables?
+    // DONE - Add instance variables?
+    private int[] intSets;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
+        // DONE
+        intSets = new int[n];
+        for (int i = 0; i < n; i += 1) {
+            intSets[i] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        // DONE
+        if (vertex >= intSets.length || vertex < 0) {
+            throw new IllegalArgumentException("Invalid index");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        // DONE
+        validate(v1);
+        return -parent(find(v1));
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        return -1;
+        // DONE
+        validate(v1);
+        // Value in intSets for each root is its tree's size.
+        return intSets[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
+        // DONE
+        validate(v1);
+        validate(v2);
+        return find(v1) == find(v2);
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -38,14 +51,42 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        // DONE
+        validate(v1);
+        validate(v2);
+        if (!connected(v1, v2)) {
+            // no extra path-compression step needed for the case of
+            // connected v1 and v2,
+            // because the connected(v1, v2) is always called whenever
+            // v1 and v2 are connected or not,
+            // because there is find() in connected(), 
+            // so path-compression is already applied.
+            if (sizeOf(v1) > sizeOf(v2)) {
+                intSets[find(v1)] -= sizeOf(v2);
+                intSets[find(v2)] = find(v1);
+            } else {
+                intSets[find(v2)] -= sizeOf(v1);
+                intSets[find(v1)] = find(v2);
+            }
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO
-        return -1;
+        // DONE
+        validate(vertex);
+        int root = vertex;
+        while (parent(root) > -1) {
+            root = parent(root);
+        }
+        // path-compression
+        int currParent;
+        while (vertex != root) {
+            currParent = parent(vertex);
+            intSets[vertex] = root;
+            vertex = currParent;
+        }
+        return root;
     }
-
 }
