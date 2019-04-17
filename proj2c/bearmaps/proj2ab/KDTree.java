437b319c45d6a73c94f8a8b0460e673ed7427d7e
zangsy <fbhduckpears@gmail.com>
2019-04-17 16:37:50 +0900

    Add previous works to help implement proj2c

--- proj2c/bearmaps/proj2ab/KDTree.java
+++ proj2c/bearmaps/proj2ab/KDTree.java
@@ -1,82 +1,64 @@
 package bearmaps.proj2ab;
 
+import java.util.Collections;
 import java.util.List;
 
 public class KDTree implements PointSet {
-
-    private TreeNode root;
+    private static final boolean HORIZONTAL = false;
+    private Node root;
 
     public KDTree(List<Point> points) {
-        root = null;
+        Collections.shuffle(points);
         for (Point point : points) {
-            insert(point);
+            root = insert(point, root, HORIZONTAL);
         }
     }
 
     // Insert points into the KDTree.
-    private void insert(Point point) {
-        root = insert(point, root, "x");
-    }
-
-    private TreeNode insert(Point point, TreeNode node, String splitDim) {
+    private Node insert(Point point, Node node, boolean splitDim) {
         if (node == null) {
-            return new TreeNode(point, splitDim);
+            return new Node(point, splitDim);
         }
 
-        double nodePos;
-        double pointPos;
+        // If point has the same coordinate as the node point,
+        // just return the node;
+        if (point.equals(node.getPoint())) {
+            return node;
+        }
 
-        if (node.getSplitDim().equals("x")) {
-            nodePos = node.getPoint().getX();
-            pointPos = point.getX();
-
-            if (pointPos < nodePos) {
-                node.left = insert(point, node.getLeft(), "y");
-            } else {
-                node.right = insert(point, node.getRight(), "y");
-            }
-
+        int cmp = comparePoints(point, node.getPoint(), splitDim);
+        if (cmp < 0) {
+            node.left = insert(point, node.getLeft(), !splitDim);
         } else {
-            nodePos = node.getPoint().getY();
-            pointPos = point.getY();
-
-            if (pointPos < nodePos) {
-                node.left = insert(point, node.getLeft(), "x");
-            } else {
-                node.right = insert(point, node.getRight(), "x");
-            }
+            node.right = insert(point, node.getRight(), !splitDim);
         }
 
         return node;
-
     }
 
     @Override
+    // Find the nearest point to the target point.
     public Point nearest(double x, double y) {
         Point target = new Point(x, y);
-        return nearest(target, root, root.getPoint(), "x");
+        return nearest(root, target, root.getPoint());
     }
 
-    private Point nearest(Point target, TreeNode node, Point best, String splitDim) {
+    private Point nearest(Node node, Point target, Point best) {
         if (node == null) {
             return best;
         }
 
-        double nodePos;
-        double targetPos;
-
-        if (node.getSplitDim().equals("x")) {
-            nodePos = node.getPoint().getX();
-            targetPos = target.getX();
-        } else {
-            nodePos = node.getPoint().getY();
-            targetPos = target.getY();
+        // Compare the current best point with the current node's point.
+        double bestDist = Point.distance(best, target);
+        double currDist = Point.distance(node.getPoint(), target);
+        if (Double.compare(currDist, bestDist) < 0) {
+            best = node.getPoint();
         }
 
-        TreeNode goodSideNode;
-        TreeNode badSideNode;
-
-        if (targetPos < nodePos) {
+        Node goodSideNode;
+        Node badSideNode;
+        int cmp = comparePoints(target, node.getPoint(), node.getSplitDim());
+        if (cmp < 0) {
             goodSideNode = node.getLeft();
             badSideNode = node.getRight();
         } else {
@@ -84,83 +66,70 @@ public class KDTree implements PointSet {
             badSideNode = node.getLeft();
         }
 
-        // Search the possible nearest point in the region where target locates.
-        double currDist = Point.distance(target, best);
-
-        if (goodSideNode != null) {
-            double goodDist = Point.distance(target, goodSideNode.getPoint());
-
-            if (goodDist < currDist) {
-                best = goodSideNode.getPoint();
-            }
-
-            if (node.getSplitDim().equals("x")){
-                best = nearest(target, goodSideNode, best, "y");
-            } else {
-                best = nearest(target, goodSideNode, best, "x");
-            }
-        }
-
-        // Search the nearest point in the another half region if possible.
-        if (badSideNode != null) {
-
-            double badX;
-            double badY;
-
-            if (node.getSplitDim().equals("x")) {
-                badX = node.getPoint().getX();
-                badY = target.getY();
-            } else {
-                badX = target.getX();
-                badY = node.getPoint().getY();
-            }
-
-            Point badPoint = new Point(badX, badY);
-            double badDist = Point.distance(target, badPoint);
-            double bestDist = Point.distance(target, best);
-
-            if (badDist < bestDist) {
-
-                if (node.getSplitDim().equals("x")) {
-                    best = nearest(target, badSideNode, best, "y");
-                } else {
-                    best = nearest(target, badSideNode, best, "x");
-                }
-            }
+        // DFS in goodSide first, then check the badSide, done recursively.
+        best = nearest(goodSideNode, target, best);
+        if (isWorthLooking(node, target, best)) {
+            best = nearest(badSideNode, target, best);
         }
 
         return best;
     }
 
-    private class TreeNode {
+    // Check whether the badSide intersects with the circle that,
+    // centred at target point with radius of square distance between
+    // target point and best point. If intersects, then the badSide is
+    // worth looking.
+    private boolean isWorthLooking(Node node, Point target, Point best) {
+        double distToBest = Point.distance(best, target);
+        double distToBad;
+        if (node.splitDim == HORIZONTAL) {
+            distToBad = Point.distance(new Point(node.point.getX(), target.getY()), target);
+        } else {
+            distToBad = Point.distance(new Point(target.getX(), node.point.getY()), target);
+        }
+        return Double.compare(distToBad, distToBest) < 0;
+    }
 
+    // Compare two points based on the split dimension of the current node.
+    private int comparePoints(Point a, Point b, boolean splitDim) {
+        if (splitDim == HORIZONTAL) {
+            return Double.compare(a.getX(), b.getX());
+        } else {
+            return Double.compare(a.getY(), b.getY());
+        }
+    }
+
+    private class Node {
         private Point point;
-        private TreeNode left;
-        private TreeNode right;
-        private String splitDim;
+        private boolean splitDim;
+        private Node left;
+        private Node right;
 
-        private TreeNode(Point point, String dim) {
+        Node(Point point, boolean splitDim) {
             this.point = point;
+            this.splitDim = splitDim;
             left = null;
             right = null;
-            splitDim = dim;
         }
 
-        private Point getPoint() {
+        public Point getPoint() {
             return point;
         }
 
-        private TreeNode getLeft() {
+        public Node getLeft() {
             return left;
         }
 
-        private TreeNode getRight() {
+        public Node getRight() {
             return right;
         }
 
-        private String getSplitDim() {
+        public boolean getSplitDim() {
             return splitDim;
         }
     }
 
+//    public static void main(String[] args) {
+//
+//    }
 }
