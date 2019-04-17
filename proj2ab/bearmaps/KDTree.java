2d4e71d2ba705169df0834d5e7f0880765aa76c2
zangsy <fbhduckpears@gmail.com>
2019-04-17 16:37:06 +0900

    Fix bugs in private nearest()

--- proj2ab/bearmaps/KDTree.java
+++ proj2ab/bearmaps/KDTree.java
@@ -1,111 +1,135 @@
 package bearmaps;
 
+import java.util.Collections;
 import java.util.List;
 
 public class KDTree implements PointSet {
-
+    private static final boolean HORIZONTAL = false;
     private Node root;
 
     public KDTree(List<Point> points) {
-        root = null;
+        Collections.shuffle(points);
         for (Point point : points) {
-            insert(point);
+            root = insert(point, root, HORIZONTAL);
         }
     }
 
     // Insert points into the KDTree.
-    private void insert(Point point) {
-        root = insert(point, root, true);
-    }
-
-    private Node insert(Point point, Node node, boolean isNodeSplitX) {
+    private Node insert(Point point, Node node, boolean splitDim) {
         if (node == null) {
-            return new Node(point);
+            return new Node(point, splitDim);
         }
-        double pointPos = isNodeSplitX ? point.getX() : point.getY();
-        double nodePos = isNodeSplitX ? node.getPoint().getX() : node.getPoint().getY();
-        if (pointPos < nodePos) {
-            node.left = insert(point, node.getLeft(), !isNodeSplitX);
+
+        // If point has the same coordinate as the node point,
+        // just return the node;
+        if (point.equals(node.getPoint())) {
+            return node;
+        }
+
+        int cmp = comparePoints(point, node.getPoint(), splitDim);
+        if (cmp < 0) {
+            node.left = insert(point, node.getLeft(), !splitDim);
         } else {
-            node.right = insert(point, node.getRight(), !isNodeSplitX);
+            node.right = insert(point, node.getRight(), !splitDim);
         }
+
         return node;
     }
 
     @Override
+    // Find the nearest point to the target point.
     public Point nearest(double x, double y) {
         Point target = new Point(x, y);
-        return nearest(target, root, root.getPoint(), true);
+        return nearest(root, target, root.getPoint());
     }
 
-    private Point nearest(Point target, Node node, Point globalNearest, boolean isNodeSplitX) {
+    private Point nearest(Node node, Point target, Point best) {
         if (node == null) {
-            return globalNearest;
-        }
-        double targetPos = isNodeSplitX ? target.getX() : target.getY();
-        double nodePos = isNodeSplitX ? node.getPoint().getX() : node.getPoint().getY();
-
-        // Search the nearest point in the region where target locates.
-        Node goodNode = targetPos < nodePos ? node.getLeft() : node.getRight();
-        Node badNode = targetPos < nodePos ? node.getRight() : node.getLeft();
-        if (goodNode != null) {
-            double currDist = Point.distance(target, globalNearest);
-            double goodDist = Point.distance(target, goodNode.getPoint());
-            if (goodDist < currDist) {
-                globalNearest = goodNode.getPoint();
-            }
-            globalNearest = nearest(target, goodNode, globalNearest, !isNodeSplitX);
+            return best;
         }
 
-        // Search the nearest point in the another half region if possible.
-        if (badNode != null) {
-            double nearestDist = Point.distance(target, globalNearest);
-            double virtualX = isNodeSplitX ? node.getPoint().getX() : target.getX();
-            double virtualY = isNodeSplitX ? target.getY() : node.getPoint().getY();
-            Point virtualPoint = new Point(virtualX, virtualY);
-            double virtualDist = Point.distance(target, virtualPoint);
-            if (virtualDist < nearestDist) {
-                globalNearest = nearest(target, badNode, globalNearest, !isNodeSplitX);
-            }
+        // Compare the current best point with the current node's point.
+        double bestDist = Point.distance(best, target);
+        double currDist = Point.distance(node.getPoint(), target);
+        if (Double.compare(currDist, bestDist) < 0) {
+            best = node.getPoint();
         }
-        return globalNearest;
+
+        Node goodSideNode;
+        Node badSideNode;
+        int cmp = comparePoints(target, node.getPoint(), node.getSplitDim());
+        if (cmp < 0) {
+            goodSideNode = node.getLeft();
+            badSideNode = node.getRight();
+        } else {
+            goodSideNode = node.getRight();
+            badSideNode = node.getLeft();
+        }
+
+        // DFS in goodSide first, then check the badSide, done recursively.
+        best = nearest(goodSideNode, target, best);
+        if (isWorthLooking(node, target, best)) {
+            best = nearest(badSideNode, target, best);
+        }
+
+        return best;
+    }
+
+    // Check whether the badSide intersects with the circle that,
+    // centred at target point with radius of square distance between
+    // target point and best point. If intersects, then the badSide is
+    // worth looking.
+    private boolean isWorthLooking(Node node, Point target, Point best) {
+        double distToBest = Point.distance(best, target);
+        double distToBad;
+        if (node.splitDim == HORIZONTAL) {
+            distToBad = Point.distance(new Point(node.point.getX(), target.getY()), target);
+        } else {
+            distToBad = Point.distance(new Point(target.getX(), node.point.getY()), target);
+        }
+        return Double.compare(distToBad, distToBest) < 0;
+    }
+
+    // Compare two points based on the split dimension of the current node.
+    private int comparePoints(Point a, Point b, boolean splitDim) {
+        if (splitDim == HORIZONTAL) {
+            return Double.compare(a.getX(), b.getX());
+        } else {
+            return Double.compare(a.getY(), b.getY());
+        }
     }
 
     private class Node {
-
         private Point point;
+        private boolean splitDim;
         private Node left;
         private Node right;
 
-        private Node(Point point) {
+        Node(Point point, boolean splitDim) {
             this.point = point;
+            this.splitDim = splitDim;
             left = null;
             right = null;
         }
 
-        private Point getPoint() {
+        public Point getPoint() {
             return point;
         }
 
-        private Node getLeft() {
+        public Node getLeft() {
             return left;
         }
 
-        private Node getRight() {
+        public Node getRight() {
             return right;
+        }
+
+        public boolean getSplitDim() {
+            return splitDim;
         }
     }
 
-    /*
-    public static void main(String[] args) {
-        Point p1 = new Point(1.1, 2.2); // constructs a Point with x = 1.1, y = 2.2
-        Point p2 = new Point(3.3, 4.4);
-        Point p3 = new Point(-2.9, 4.2);
-        KDTree kdTree = new KDTree(List.of(p1, p2, p3));
-
-        Point nearest = kdTree.nearest(3.0, 4.0); // returns p2
-        System.out.println(nearest.getX() + " and " + nearest.getY()); // evaluates to 3.3 and 4.4;
-    }
-    */
-
+//    public static void main(String[] args) {
+//
+//    }
 }
